b1441409dcfa646dd1e6f1cc8e25b802e26aeca8
mariepierreMangeret <mangeret.mariepierre@gmail.com>
2018-08-06 17:55:50 +0200

    ajout de la section nouveautes

--- src/JSC/PlatformBundle/Controller/PlatformController.php
+++ src/JSC/PlatformBundle/Controller/PlatformController.php
@@ -3,11 +3,25 @@
 namespace JSC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use JSC\PlatformBundle\Entity\News;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 class PlatformController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('JSCPlatformBundle::index.html.twig');
+    	$nbPerPage = 3;
+
+        $news = $this
+          ->getDoctrine()
+          ->getManager()
+          ->getRepository('JSCPlatformBundle:News')
+          ->myNewsIndex($nbPerPage);
+
+        return $this->render(
+        	'JSCPlatformBundle::index.html.twig',array(
+        	'news'		=> $news,
+        ));
     }
 }
---
+++ src/JSC/PlatformBundle/Entity/News.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace JSC\PlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * News
+ *
+ * @ORM\Table(name="jsc_news")
+ * @ORM\Entity(repositoryClass="JSC\PlatformBundle\Repository\NewsRepository")
+ */
+class News
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime", nullable=true)
+     */
+    private $date;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return news
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return news
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return news
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+}
+
