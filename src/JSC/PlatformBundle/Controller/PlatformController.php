6ecb2f01b03ec2d90329d4edd8544f5e89c9f86d
mariepierreMangeret <mangeret.mariepierre@gmail.com>
2018-08-12 16:53:16 +0200

    Ajout envoi de mail formulaire contact

--- src/JSC/PlatformBundle/Controller/PlatformController.php
+++ src/JSC/PlatformBundle/Controller/PlatformController.php
@@ -34,16 +34,57 @@ class PlatformController extends Controller
         ->myEventsIndex($nbEventPerPage);
 
       $models = $this
-      ->getDoctrine()
-      ->getManager()
-      ->getRepository('JSCPlatformBundle:Model')
-      ->myModelsIndex();
+        ->getDoctrine()
+        ->getManager()
+        ->getRepository('JSCPlatformBundle:Model')
+        ->myModelsIndex();
 
       $brands = $this
-      ->getDoctrine()
-      ->getManager()
-      ->getRepository('JSCPlatformBundle:Brand')
-      ->myBrandsIndex();
+        ->getDoctrine()
+        ->getManager()
+        ->getRepository('JSCPlatformBundle:Brand')
+        ->myBrandsIndex();
+
+      // Envoyer formulaire de contact par email 
+      $form = $this->createForm('JSC\PlatformBundle\Form\ContactType',null,array(
+          // To set the action use $this->generateUrl('route_identifier')
+          'action' => $this->generateUrl('jsc_platform_home'),
+          'method' => 'POST'
+      ));
+      if ($request->isMethod('POST')) {
+        $form->handleRequest($request);
+
+          if($form->isValid()){
+
+
+            $message = (new \Swift_Message('Formulaire de contact'))
+              ->setFrom('japanstylecollection@gmail.com')
+              ->setTo('japanstylecollection@gmail.com')
+              ->setBody(
+                  $this->renderView(
+                      // app/Resources/views/emails/contact.html.twig
+                      'emails/contact.html.twig',
+                      array(
+                        'name'     => $form["name"]->getData(),
+                        'email'    => $form["email"]->getData(),
+                        'phone'    => $form["phone"]->getData(),
+                        'subject'  => $form["subject"]->getData(),
+                        'message'  => $form["message"]->getData()
+                    )
+                  ),
+                  'text/html'
+              )
+            ;
+          
+            $this->get('mailer')->send($message);
+            unset($form);
+            $form = $this->createForm('JSC\PlatformBundle\Form\ContactType',null,array(
+                // To set the action use $this->generateUrl('route_identifier')
+                'action' => $this->generateUrl('jsc_platform_home'),
+                'method' => 'POST'
+            ));
+          }
+      }
 
       return $this->render(
       	'JSCPlatformBundle::index.html.twig',array(
@@ -51,6 +92,7 @@ class PlatformController extends Controller
         'events'  => $events,
         'models'  => $models,
         'brands'  => $brands,
+        'form' => $form->createView(),
       ));
     }
 }
